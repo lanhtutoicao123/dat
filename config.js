2e6aa958b39bedbffc97ccb6ec8fce1f5825bc9c
lanhtutoicao123 <94688850+lanhtutoicao123@users.noreply.github.com>
2021-11-19 21:53:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'thích Em nhất <33',
+    btnYes: 'Vâng yêu Em nhất <33',
     btnNo: 'Không, em gái à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
